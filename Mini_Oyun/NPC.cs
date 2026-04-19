c22749ae6ae4635293cc2bf8205695e7df1e53f9
Oğuz <oguz.selvin@gmail.com>
2026-04-19 17:35:58 +0300

    Npc Diyalog
    Npc'ler için hikaye de oluşacak diyalog kısımları eklenecek.

---
+++ Mini_Oyun/NPC.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Oyun
+{
+    public class NPC
+    {
+        public string Ad { get; set; }
+        public string Rol { get; set; }
+        public string[] Diyaloglar { get; set; }
+
+        public NPC(string ad, string rol, string[] diyaloglar)
+        {
+            Ad = ad;
+            Rol = rol;
+            Diyaloglar = diyaloglar;
+        }
+
+        public void Konus()
+        {
+            Random rnd = new Random();
+            string secilenDiyalog = Diyaloglar[rnd.Next(Diyaloglar.Length)];
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"\n[{Ad} - {Rol}]: \"{secilenDiyalog}\"");
+            Console.ResetColor();
+        }
+    }
+}
