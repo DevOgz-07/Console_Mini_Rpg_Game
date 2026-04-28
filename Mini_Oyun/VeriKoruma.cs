433f00a83a12c54dcb02d34a25cbb2a9ecf42bb5
Oğuz <oguz.selvin@gmail.com>
2026-04-29 01:47:44 +0300

    Veri Koruma Ve Hashleme
    Save Dosyası Hash sistemi getirildi.
    
    Hash değiştirilmeye çalışıldığı esnada ise save dosyasını silip sıfırdan yeni karakter oluşturacak şekilde ayarlandı. Save dosyası korundu.

---
+++ Mini_Oyun/VeriKoruma.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public static class VeriKoruma
+{
+    private const string GizliAnahtar = "Gumusisik_Sirlari_2026_Secure!";
+
+    
+    public static string HashHesapla(string veri)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(veri + GizliAnahtar));
+            return BitConverter.ToString(bytes).Replace("-", "").ToLower();
+        }
+    }
+
+    
+    public static string Kodla(string veri) => Convert.ToBase64String(Encoding.UTF8.GetBytes(veri));
+
+    
+    public static string Coz(string kodlanmisVeri) => Encoding.UTF8.GetString(Convert.FromBase64String(kodlanmisVeri));
+}
