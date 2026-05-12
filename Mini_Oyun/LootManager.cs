18a6126f35a48d8b337886e94c8eb27ae160774f
Oğuz <oguz.selvin@gmail.com>
2026-05-12 13:12:41 +0300

    Update Loot
    Loot Sistemi Güncellendi

--- Mini_Oyun/LootManager.cs
+++ Mini_Oyun/LootManager.cs
@@ -12,50 +12,42 @@ namespace Mini_Oyun
 
 
         private static Dictionary<string, List<Oge>> LootTables = new Dictionary<string, List<Oge>>
+{
+    {
+        "GENEL_COMMON",
+        new List<Oge>
         {
-            {
-                "GENEL_COMMON", //Tüm Common Canavarlar Bu itemleri düşürecek
-                new List<Oge>
-                {
-                    new Oge("Küçük Can İksiri", Nadirlik.Common, OgeTuru.Tuketilebilir, 25),
-                    new Oge("Orta Can İksiri", Nadirlik.Common, OgeTuru.Tuketilebilir, 50),
-                    new Oge("Büyük Can İkisiri", Nadirlik.Common, OgeTuru.Tuketilebilir, 100),
-                    new Oge("Demir Kılıç", Nadirlik.Common, OgeTuru.Silah, 5),
-                    new Oge("Derinlik Zırhı", Nadirlik.Common, OgeTuru.Zirh, 3),
-                    new Oge("Hilal Kılıcı", Nadirlik.Common, OgeTuru.Silah, 7),
-                    new Oge("Zincir Zırh", Nadirlik.Common, OgeTuru.Zirh, 5),
-                    new Oge("Azap Kılıcı", Nadirlik.Common, OgeTuru.Silah, 10),
-                    new Oge("Kara Zırh", Nadirlik.Common, OgeTuru.Zirh, 7),
-                    new Oge("Uzun Kılıç", Nadirlik.Common, OgeTuru.Silah, 8),
-                    new Oge("Kısa Kılıç", Nadirlik.Common, OgeTuru.Zirh, 6),
+            // Format: Ad, Nadirlik, Tür, EtkiDegeri(Min), MaxEtkiDegeri(Max), GerekenSeviye
+            new Oge("Küçük Can İksiri", Nadirlik.Common, OgeTuru.Tuketilebilir, 25, 25, 1),
+            new Oge("Demir Kılıç", Nadirlik.Common, OgeTuru.Silah, 5, 9, 1),
+            new Oge("Derinlik Zırhı", Nadirlik.Common, OgeTuru.Zirh, 3, 3, 1),
+            new Oge("Hilal Kılıcı", Nadirlik.Common, OgeTuru.Silah, 7, 12, 3),
+            new Oge("Zincir Zırh", Nadirlik.Common, OgeTuru.Zirh, 5, 5, 4),
+            new Oge("Uzun Kılıç", Nadirlik.Common, OgeTuru.Silah, 8, 14, 5),
+            new Oge("Kısa Kılıç", Nadirlik.Common, OgeTuru.Silah, 6, 10, 2) 
+        }
+    },
+    {
+        "GENEL_RARE",
+        new List<Oge>
+        {
+            new Oge("Keskin Çelik Kılıç", Nadirlik.Rare, OgeTuru.Silah, 12, 18, 8),
+            new Oge("Gümüş Kaplama Zırh", Nadirlik.Rare, OgeTuru.Zirh, 8, 8, 10),
+            new Oge("Güçlendirilmiş Kalkan", Nadirlik.Rare, OgeTuru.Zirh, 10, 10, 7)
+        }
+    },
+    {
+        "GENEL_BOSS",
+        new List<Oge>
+        {
+            new Oge("Ejderha Kılıcı", Nadirlik.Epic, OgeTuru.Silah, 20, 32, 15),
+            new Oge("Ejderha Zırhı", Nadirlik.Epic, OgeTuru.Zirh, 15, 15, 15)
+        }
+    },
+};
 
 
-                }
-            },
-            {
-                "GENEL_RARE", //Tüm Rare Canavarlar Bu itemleri düşürecek
-                new List<Oge>
-                {
-                    new Oge("Keskin Çelik Kılıç", Nadirlik.Rare, OgeTuru.Silah, 12),
-                    new Oge("Gümüş Kaplama Zırh", Nadirlik.Rare, OgeTuru.Zirh, 8),
-                    new Oge("Büyük Şifa İksiri", Nadirlik.Rare, OgeTuru.Tuketilebilir, 40),
-                    new Oge("Güçlendirilmiş Kalkan", Nadirlik.Rare, OgeTuru.Zirh, 10)
-                }
-            },
-            {
-                "GENEL_BOSS", // Tüm Boss Canavarlar Bu itemleri düşürecek
-                new List<Oge>
-                {
-                    new Oge("Ejderha Kılıcı", Nadirlik.Epic, OgeTuru.Silah, 20),
-                    new Oge("Ejderha Zırhı", Nadirlik.Epic, OgeTuru.Zirh, 15),
-                    new Oge("Büyük Can İksiri", Nadirlik.Rare, OgeTuru.Tuketilebilir, 50),
-                    new Oge("Büyük Şifa İksiri", Nadirlik.Rare, OgeTuru.Tuketilebilir, 40)
-                }
-            },
-        };
-        
 
-        
         public static List<Oge> LootDusur(Canavar canavar)
         {
             switch (canavar.Turu)
