fcdd941d2376712825668e8cde90666e9734ac88
Oğuz <oguz.selvin@gmail.com>
2026-05-20 13:11:54 +0300

    Loot Update
    Loot ve Maneger sistemi düzenlendi

--- Mini_Oyun/LootEntry.cs
+++ Mini_Oyun/LootEntry.cs
@@ -6,15 +6,24 @@ using System.Threading.Tasks;
 
 namespace Mini_Oyun
 {
-    public class LootEntry
+    public sealed class LootEntry
     {
-        public Oge Oge { get; set; }
-        public int DropSans { get; set; } // yüzde
+       
+        public Oge Oge { get; }
+        public int DropSans { get; } 
 
         public LootEntry(Oge oge, int dropSans)
         {
-            Oge = oge;
+            
+            Oge = oge ?? throw new ArgumentNullException(nameof(oge), "Loot girdisi için geçerli bir 'Oge' nesnesi sağlanmalıdır.");
+
+            if (dropSans < 0 || dropSans > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(dropSans), "Düşme şansı (DropSans) 0 ile 100 arasında bir yüzde değeri olmalıdır.");
+            }
+
             DropSans = dropSans;
         }
+        public double GetDropRate() => DropSans / 100.0;
     }
 }
--- Mini_Oyun/LootManager.cs
+++ Mini_Oyun/LootManager.cs
@@ -1,82 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mini_Oyun
 {
     internal static class LootManager
     {
-        private static Random random = new Random();
+        private static readonly Random _random = new Random();
 
+        private static readonly Dictionary<string, List<LootEntry>> LootTables = new Dictionary<string, List<LootEntry>>();
 
-        private static Dictionary<string, List<Oge>> LootTables = new Dictionary<string, List<Oge>>
-{
-    {
-        "GENEL_COMMON",
-        new List<Oge>
+        static LootManager()
         {
-            // Format: Ad, Nadirlik, Tür, EtkiDegeri(Min), MaxEtkiDegeri(Max), GerekenSeviye
-            new Oge("Küçük Can İksiri", Nadirlik.Common, OgeTuru.Tuketilebilir, 25, 25, 1),
-            new Oge("Demir Kılıç", Nadirlik.Common, OgeTuru.Silah, 5, 9, 1),
-            new Oge("Derinlik Zırhı", Nadirlik.Common, OgeTuru.Zirh, 3, 3, 1),
-            new Oge("Hilal Kılıcı", Nadirlik.Common, OgeTuru.Silah, 7, 12, 3),
-            new Oge("Zincir Zırh", Nadirlik.Common, OgeTuru.Zirh, 5, 5, 4),
-            new Oge("Uzun Kılıç", Nadirlik.Common, OgeTuru.Silah, 8, 14, 5),
-            new Oge("Kısa Kılıç", Nadirlik.Common, OgeTuru.Silah, 6, 10, 2) 
+            SeedLootTables();
         }
-    },
-    {
-        "GENEL_RARE",
-        new List<Oge>
-        {
-            new Oge("Keskin Çelik Kılıç", Nadirlik.Rare, OgeTuru.Silah, 12, 18, 8),
-            new Oge("Gümüş Kaplama Zırh", Nadirlik.Rare, OgeTuru.Zirh, 8, 8, 10),
-            new Oge("Güçlendirilmiş Kalkan", Nadirlik.Rare, OgeTuru.Zirh, 10, 10, 7)
-        }
-    },
-    {
-        "GENEL_BOSS",
-        new List<Oge>
-        {
-            new Oge("Ejderha Kılıcı", Nadirlik.Epic, OgeTuru.Silah, 20, 32, 15),
-            new Oge("Ejderha Zırhı", Nadirlik.Epic, OgeTuru.Zirh, 15, 15, 15)
-        }
-    },
-};
 
+        private static void SeedLootTables()
+        {
+            // COMMON HAVUZU
+            LootTables.Add("GENEL_COMMON", new List<LootEntry>
+            {
+                new LootEntry(new Oge("Küçük Can İksiri", Nadirlik.Common, OgeTuru.Tuketilebilir, 25, 25, 1), 60),
+                new LootEntry(new Oge("Demir Kılıç", Nadirlik.Common, OgeTuru.Silah, 5, 9, 1), 30),
+                new LootEntry(new Oge("Derinlik Zırhı", Nadirlik.Common, OgeTuru.Zirh, 3, 3, 1), 25),
+                new LootEntry(new Oge("Kısa Kılıç", Nadirlik.Common, OgeTuru.Silah, 6, 10, 2), 40),
+                new LootEntry(new Oge("Hilal Kılıcı", Nadirlik.Common, OgeTuru.Silah, 7, 12, 3), 20),
+                new LootEntry(new Oge("Zincir Zırh", Nadirlik.Common, OgeTuru.Zirh, 5, 5, 4), 15),
+                new LootEntry(new Oge("Uzun Kılıç", Nadirlik.Common, OgeTuru.Silah, 8, 14, 5), 10)
+            });
 
+            // RARE HAVUZU
+            LootTables.Add("GENEL_RARE", new List<LootEntry>
+            {
+                new LootEntry(new Oge("Keskin Çelik Kılıç", Nadirlik.Rare, OgeTuru.Silah, 12, 18, 8), 25),
+                new LootEntry(new Oge("Gümüş Kaplama Zırh", Nadirlik.Rare, OgeTuru.Zirh, 8, 8, 10), 15),
+                new LootEntry(new Oge("Güçlendirilmiş Kalkan", Nadirlik.Rare, OgeTuru.Zirh, 10, 10, 7), 20)
+            });
+
+            // BOSS HAVUZU
+            LootTables.Add("GENEL_BOSS", new List<LootEntry>
+            {
+                new LootEntry(new Oge("Ejderha Kılıcı", Nadirlik.Epic, OgeTuru.Silah, 20, 32, 15), 40),
+                new LootEntry(new Oge("Ejderha Zırhı", Nadirlik.Epic, OgeTuru.Zirh, 15, 15, 15), 35)
+            });
+        }
 
         public static List<Oge> LootDusur(Canavar canavar)
         {
+            if (canavar == null) return new List<Oge>();
+
             switch (canavar.Turu)
             {
                 case CanavarTuru.CommonMonster:
                     return GenelLootGetir("GENEL_COMMON");
-
                 case CanavarTuru.RareMonster:
                     return GenelLootGetir("GENEL_RARE");
-
                 case CanavarTuru.Boss:
-                    
                     return BossLootDusur(canavar.LootTableId);
-
                 default:
                     return new List<Oge>();
             }
         }
 
-        
         private static List<Oge> GenelLootGetir(string poolKey)
         {
             List<Oge> dusenler = new List<Oge>();
 
-            if (LootTables.ContainsKey(poolKey))
+            if (LootTables.TryGetValue(poolKey, out var tablo))
             {
-                var tablo = LootTables[poolKey];
-                
-                dusenler.Add(tablo[random.Next(tablo.Count)]);
+                var karisikTablo = tablo.OrderBy(_ => _random.Next()).ToList();
+
+                foreach (var entry in karisikTablo)
+                {
+                    if (_random.NextDouble() <= entry.GetDropRate())
+                    {
+                        dusenler.Add(entry.Oge);
+                        break;
+                    }
+                }
             }
 
             return dusenler;
@@ -86,21 +87,19 @@ namespace Mini_Oyun
         {
             List<Oge> dusenLootlar = new List<Oge>();
 
-           
-            if (!LootTables.ContainsKey(lootTableKey))
-            {
-                return GenelLootGetir("GENEL_RARE");
-            }
+            string activeKey = (!string.IsNullOrEmpty(lootTableKey) && LootTables.ContainsKey(lootTableKey))
+                ? lootTableKey
+                : "GENEL_RARE";
 
-            var tablo = LootTables[lootTableKey];
-            var kopyaTablo = new List<Oge>(tablo);
+            var tablo = LootTables[activeKey];
 
-            
-            for (int i = 0; i < 2 && kopyaTablo.Count > 0; i++)
+            var kopyaTablo = tablo.OrderByDescending(e => e.DropSans * _random.NextDouble()).ToList();
+
+            int limitselSecimSayisi = Math.Min(2, kopyaTablo.Count);
+
+            for (int i = 0; i < limitselSecimSayisi; i++)
             {
-                int index = random.Next(kopyaTablo.Count);
-                dusenLootlar.Add(kopyaTablo[index]);
-                kopyaTablo.RemoveAt(index);
+                dusenLootlar.Add(kopyaTablo[i].Oge);
             }
 
             return dusenLootlar;
